942ad6f0d0e54adc723b613428fe470e411952d4
richard85501 <liuchang85501@gmail.com>
2023-10-20 00:38:05 +0800

    feat add collect file

---
+++ pages/collect/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout/layout';
+
+const collect = () => {
+  return (
+    <Layout>
+      <div>this is collect page</div>;
+    </Layout>
+  );
+};
+
+export default collect;
