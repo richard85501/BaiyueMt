19b7da2b1dd7863e9dcfb95bc0169dafaabea82a
richard85501 <liuchang85501@gmail.com>
2023-10-20 00:37:45 +0800

    feat add catogries file

---
+++ pages/categories/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout/layout';
+
+const profit = () => {
+  return (
+    <Layout>
+      <div>this is 類別資料</div>;
+    </Layout>
+  );
+};
+
+export default profit;
