b1738959bb1cfb4df522950c7df8e34ab221a34d
richard85501 <liuchang85501@gmail.com>
2023-10-20 00:38:21 +0800

    feat add ghost file

---
+++ pages/ghost/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@/components/layout/layout';
+
+const stocksPage = () => {
+  return (
+    <Layout>
+      <div>this is 百岳一覽</div>
+    </Layout>
+  );
+};
+
+export default stocksPage;
